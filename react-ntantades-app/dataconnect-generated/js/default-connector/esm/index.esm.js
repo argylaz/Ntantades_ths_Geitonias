d827872a747244cf17c7e2142736d8681195e831
argylaz <argyrhslazaridhs@gmail.com>
2024-12-19 14:05:11 +0200

    Reinstalled firebase to only have hosting feature

--- react-ntantades-app/dataconnect-generated/js/default-connector/esm/index.esm.js
+++
@@ -1,8 +0,0 @@
-import { getDataConnect, validateArgs } from 'firebase/data-connect';
-
-export const connectorConfig = {
-  connector: 'default',
-  service: 'react-ntantades-app',
-  location: 'us-central1'
-};
-
--- react-ntantades-app/src/App.js
+++ react-ntantades-app/src/App.js
@@ -1,5 +1,5 @@
 import './StyleSheets/App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Routes, Route } from 'react-router-dom';
 import Header from './components/header.jsx';
 import Footer from './components/footer.jsx';
 import Home from './pages/HomePage.jsx';
